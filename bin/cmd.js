6faf348509498797360ad20492fac6bb1ed1d93d
sterpe <srterpe@gmail.com>
2015-06-13 14:50:56 -0700

    final fixes

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,89 +1,26 @@
 #!/usr/bin/env node
-var minimist = require('minimist')
-, argv = minimist(process.argv.slice(2))
-, LineByLineReader = require('line-by-line')
-, child_process = require('child_process')
-, exec = child_process.exec
+var argv = require('minimist-cheatsheet')({
+	string: ['outfile']
+	, boolean: ['xml']
+	, alias: { h: 'help', v: 'version', o: 'outfile', c: 'clean', x: 'xml' }
+})
 , path = require('path')
 , fs = require('fs')
-, edCommand = require('../main.js')
-, range = []
-, sourceLines = []
-, targetLines = []
-, tsv
-, importFile
-, filename
-, newFilename
+, clean = []
 ;
-
-
-// Implement bash string escaping.
-var safePattern =    /^[a-z0-9_\/\-.,?:@#%^+=\[\]]*$/i;
-var safeishPattern = /^[a-z0-9_\/\-.,?:@#%^+=\[\]{}|&()<>; *']*$/i;
-function bashEscape(arg) {
-  // These don't need quoting
-  if (safePattern.test(arg)) return arg;
- 
-  // These are fine wrapped in double quotes using weak escaping.
-  if (safeishPattern.test(arg)) return '"' + arg + '"';
- 
-  // Otherwise use strong escaping with single quotes
-  return "'" + arg.replace(/'+/g, function (val) {
-    // But we need to interpolate single quotes efficiently
- 
-    // One or two can simply be '\'' -> ' or '\'\'' -> ''
-    if (val.length < 3) return "'" + val.replace(/'/g, "\\'") + "'";
- 
-    // But more in a row, it's better to wrap in double quotes '"'''''"' -> '''''
-    return "'\"" + val + "\"'";
- 
-  }) + "'";
+if (argv._.length !== 2) {
+	cmd = path.basename(process.argv[1]);
+	usage = __dirname + '/../help.txt';
+	console.log(
+		fs.readFileSync(usage, 'utf-8')
+		.replace(/\$0/g, cmd)
+		.trim()
+	);
+	process.exit();
 }
-tsv = bashEscape(argv.tsv);
-importFile = bashEscape(argv.importFile);
-
-// Need to convert the importFile file into pretty print xml
-filename = path.basename(argv.importFile, path.extname(argv.importFile));
-newFilename = filename + '-fire-drill' + path.extname(argv.importFile);
-exec('cat ' + importFile + ' | xmllint --format -', 
-	function (error, stdout, stderr) {
-		console.log('stdout:  ' + stdout);
-		console.log('stderr:  ' + stderr);
-		if (error !== null) {
-			console.log('exec error:  ' + error);
-		}
-		fs.writeFile(newFilename,
-			stdout.toString(),
-			function (err) {
-				if (err !== null) {
-					console.log(error);
-					process.exit(error);
-				}
-				processTsv();
-		});
-});
-
-function processTsv() {
-	lr = new LineByLineReader(tsv);
-	
-	lr.on('error', function (err) {
-		console.log(err);
-		process.exit(err);
-	});
-	lr.on('line', function (line) {
-		var columns = [range, sourceLines, targetLines];
-		var line = line.split("\t");
-		for (var i = 0; i < line.length; i++) {
-			columns[i].push(line[i]);
-		}
-	});
-	lr.on('end', function () {
-		console.log(range, sourceLines, targetLines);
-		edCommand(bashEscape(newFilename)
-			, range
-			, sourceLines
-			, targetLines
-			, bashEscape
-		);
-	});
+if (argv.clean) {
+	for (i = 0; i < argv.clean.length; ++i) {
+		clean[i] = JSON.parse(argv.clean[i]);
+	}
 }
+require('../main')(argv._[0], argv._[1], argv.outfile, { isXML: argv.xml, clean: clean });
--- main.js
+++ main.js
@@ -1,30 +1,32 @@
-module.exports = function (filename, RANGE, SRC, TGT, bashEscape) {
-	var child_process = require('child_process')
-	, exec = child_process.exec
-	, i
-	, s = require('./lib/s-command').s
-	, edCommands = ['H'];
+var xmllint = require('./lib/xmllint').xmllint
+, tsv = require('./lib/tsv')
+, s_replace = require('./lib/ed').s_replace
+;
+module.exports = function (tsvfile, filename, outfile, options) {
+	var XXX
+	, YYY
+	, write = !!outfile
 	;
-	
-	
-	for (i = 0; i < SRC.length; ++i) {
-		edCommands.push(s(RANGE[i], SRC[i], TGT[i], bashEscape));
+	XXX = function (search) {
+		s_replace(outfile || filename, search, write, options);
 	};
-	
-	
-	edCommands.push('w');
-	edCommands.push(',p');
-	edCommands.push('q');
-	for (i = 0; i < edCommands.length; i++) {
-		console.log("%d:	%s", i, edCommands[i]);
-	}
-	exec('printf \'%s\n\' ' +
-		edCommands.join(" ") +
-		" | /bin/ed " + filename, function (err, stdout, stderr) {
-			console.log("stdout: ", stdout);
-			console.log("stderr: ", stderr);
-			if (err !== null) {
-				console.log('exec error: ' + err);
+	YYY = function () {
+		tsv(tsvfile, options, XXX);
+	};
+	if (options.isXML) {
+		xmllint(filename, outfile, function (err, file_handle) {
+			if (err) {
+				console.log(err);
+				process.exit();
 			}
-	});
+			if (file_handle) {
+				outfile = file_handle.path;
+				options.file_handle = file_handle;
+			}
+			YYY();
+		});
+	}
+	else {
+		YYY();
+	}
 };
